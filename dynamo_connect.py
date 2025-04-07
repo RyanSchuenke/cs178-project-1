317995d71a890a85be10cbc22770889dec56a81d
SchuenkeEC2 <ryan.schuenke17@gmail.com>
2025-04-07 23:07:11 +0000

    updated attribute names, removed text interface

--- dynamo_connect.py
+++ dynamo_connect.py
@@ -13,12 +13,17 @@ table = dynamodb.Table(sql_credentials.dynamoDB_table)
 
 def create_user(username, password):
     """Create new user in table"""
-    table.put_item( 
-        Item={
-            'Username': username,
-            'Password': password,
-        }
-    )
+    try: 
+        table.put_item( 
+            Item={
+                'username': username,
+                'password': password,
+            }
+        )
+        return True
+    except botocore.exceptions.ClientError:
+        flash("username taken", "error")
+        return False
 
 
 def update_password(username, password, new_password):
@@ -26,23 +31,23 @@ def update_password(username, password, new_password):
     try: 
         table.update_item(
             Key = {
-            'Username': username,
-            'Password': password,
+            'username': username,
+            'password': password,
             }, 
-            UpdateExpression = "SET Password = :r", ExpressionAttributeValues = {':r': new_password}
+            UpdateExpression = "SET password = :r", ExpressionAttributeValues = {':r': new_password}
         )
         flash("password updated", "success")
         return True
     except botocore.exceptions.ClientError:
-        flash("Movie not in database", "error")
+        flash("user not in database", "error")
         return False
 
 
-def delete_user(user_id):
+def delete_user(username):
     """delete user from database"""
     try:
         table.delete_item(
-            Key = {"userID": user_id}
+            Key = {"username": username}
         )
         flash("user deleted", "success")
         return True
@@ -56,48 +61,16 @@ def query_login(username, password):
     try:
         response = table.get_item(
             Key = {
-                "Username": username, 
-                "Password": password
+                "username": username, 
+                "password": password
             }
         )
         user = response.get("Item")
         if user == None:
             flash("Username/Password is incorrect", "error")
-            return -1
+            return False
         else:
-            return user["userID"]
+            return True
     except botocore.exceptions.ClientError:
-        flash("Username/Password is incorrect", error) 
-        return -1
-
-def print_menu():
-    print("----------------------------")
-    print("Press C: to CREATE a new movie")
-    print("Press R: to READ all movies")
-    print("Press U: to UPDATE a new movie (add a review)")
-    print("Press D: to DELETE a movie")
-    print("Press Q: to Query a movie's average ratings")
-    print("Press X: to EXIT application")
-    print("----------------------------")
-
-
-def main():
-    input_char = ""
-    while input_char.upper() != "X":
-        print_menu()
-        input_char = input("Choice: ")
-        if input_char.upper() == "C":
-            create_movie()
-        elif input_char.upper() == "R":
-            print_all_movies()
-        elif input_char.upper() == "U":
-            update_rating()
-        elif input_char.upper() == "D":
-            delete_movie()
-        elif input_char.upper() == "Q":
-            query_movie()
-        elif input_char.upper() == "X":
-            print("exiting...")
-        else:
-            print('Not a valid option. Try again.')
-main()
+        flash("Username/Password is incorrect", "error") 
+        return False
